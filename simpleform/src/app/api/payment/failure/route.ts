d94adfa26f163c1e2783ab4dfec7a020e56eb2eb
deepak <deepakdpyqaz@gmail.com>
2024-12-31 08:06:52 +0530

    changing payment gateway method

--- simpleform/src/app/api/payment/failure/route.ts
+++ simpleform/src/app/api/payment/failure/route.ts
@@ -6,24 +6,24 @@ import Slot, { ISlot } from "../../models/Slot";
 import Transaction, { ITransaction } from "../../models/Transaction";
 import { AnyBulkWriteOperation } from "mongoose";
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
+export async function POST(req: NextRequest): Promise<NextResponse> {
     try {
         await connectToDatabase();
-        const { searchParams } = new URL(req.url);
-        const key = searchParams.get('key');
-        const txnid = searchParams.get('txnid');
-        const amount = searchParams.get('amount');
-        const productinfo = searchParams.get('productinfo');
-        const email = searchParams.get('email');
-        const firstname = searchParams.get('firstname');
-        const surl = searchParams.get('surl');
-        const furl = searchParams.get('furl');
-        const phone = searchParams.get('phone');
-        const status = searchParams.get('status');
-        const receivedHash = searchParams.get('hash');
-        const mihpayid = searchParams.get('mihpayid');
-        const mode = searchParams.get('mode');
-        const bank_ref_num = searchParams.get('bank_ref_num');
+        const body = await req.json();
+        const key = body['key'];
+        const txnid = body['txnid'];
+        const amount = body['amount'];
+        const productinfo = body['productinfo'];
+        const email = body['email'];
+        const firstname = body['firstname'];
+        const surl = body['surl'];
+        const furl = body['furl'];
+        const phone = body['phone'];
+        const status = body['status'];
+        const receivedHash = body['hash'];
+        const mihpayid = body['mihpayid'];
+        const mode = body['mode'];
+        const bank_ref_num = body['bank_ref_num'];
         if (key && txnid && amount && productinfo && productinfo && email && firstname && surl && furl && phone && status) {
             const hash = generateReverseHash(key, txnid, Number(amount), productinfo, firstname, email, status);
             if (hash !== receivedHash) {
--- simpleform/src/app/api/payment/success/route.ts
+++ simpleform/src/app/api/payment/success/route.ts
@@ -17,24 +17,24 @@ const MAIL_HTML=`
 <p>Please accept my humble obeisances. All glories to Srila Prabhupada.</p>
 <p>Thank you for registering for the Saranagati Retreat (Feb 23 - Mar 1)</p>
 `;
-export async function GET(req: NextRequest): Promise<NextResponse> {
+export async function POST(req: NextRequest): Promise<NextResponse> {
     try {
         await connectToDatabase();
-        const { searchParams } = new URL(req.url);
-        const key = searchParams.get('key');
-        const txnid = searchParams.get('txnid');
-        const amount = searchParams.get('amount');
-        const productinfo = searchParams.get('productinfo');
-        const email = searchParams.get('email');
-        const firstname = searchParams.get('firstname');
-        const surl = searchParams.get('surl');
-        const furl = searchParams.get('furl');
-        const phone = searchParams.get('phone');
-        const status = searchParams.get('status');
-        const receivedHash = searchParams.get('hash');
-        const mihpayid = searchParams.get('mihpayid');
-        const mode = searchParams.get('mode');
-        const bank_ref_num = searchParams.get('bank_ref_num');
+        const body = await req.json();
+        const key = body['key'];
+        const txnid = body['txnid'];
+        const amount = body['amount'];
+        const productinfo = body['productinfo'];
+        const email = body['email'];
+        const firstname = body['firstname'];
+        const surl = body['surl'];
+        const furl = body['furl'];
+        const phone = body['phone'];
+        const status = body['status'];
+        const receivedHash = body['hash'];
+        const mihpayid = body['mihpayid'];
+        const mode = body['mode'];
+        const bank_ref_num = body['bank_ref_num'];
         if (key && txnid && amount && productinfo && productinfo && email && firstname && surl && furl && phone && status) {
             const hash = generateReverseHash(key, txnid, Number(amount), productinfo, firstname, email, status);
             if (hash !== receivedHash) {
